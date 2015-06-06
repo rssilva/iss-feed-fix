139cdfad297087d74d8f5abd74755316d4d14578
rssilva <rafaelspdasilva@gmail.com>
2015-06-06 19:35:28 -0300

    adding router to manage different countries and cities

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 'use strict';
 
+const port = 3000;
+
 let http = require('http');
-var port = 3000;
+let Router = require('node-simple-router');
 
-http.createServer(function (req, res) {
-  res.write('Hello!');
+let router = new Router();
+
+let Parser = require('./app/Parser')(router);
+
+http.createServer(router).listen(port);
+
+console.log('listening on ', port);
+
+router.get('/', (req, res) => {
+  res.write('Hello!')
   res.end();
-}).listen(port);
-
-console.log('listening ', port);
+});
