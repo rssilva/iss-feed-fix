e92cc8d3bc8190c820137ec5cabdb6bef5c4ea02
rssilva <rafaelspdasilva@gmail.com>
2015-06-06 21:34:04 -0300

    tryin a new approach

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 'use strict';
 
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
-let http = require('http');
-let Router = require('node-simple-router');
+var http = require('http');
+var Router = require('node-simple-router');
 
-let router = new Router();
-
-let Parser = require('./app/Parser')(router);
+var router = new Router();
 
 http.createServer(router).listen(port);
 
 console.log('listening on ', port);
 
-router.get('/', (req, res) => {
-  res.write('Hello!')
+router.get('/', function (req, res) {
+  res.write('Hello!');
   res.end();
 });
